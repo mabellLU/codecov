02d23858bc34c6b99fcd56ce886c9088389b2d7e
Mike Abell <mike.abell@lendup.com>
2018-08-06 12:13:26 -0700

    more stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './Welcome';
 
 class App extends Component {
   constructor(props){
@@ -19,7 +20,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.welcome}</h1>
+          <Welcome>{this.state.welcome}</Welcome>
         </header>
         <p className="App-intro">
           <label htmlFor="welcome">Say Hey!</label>
